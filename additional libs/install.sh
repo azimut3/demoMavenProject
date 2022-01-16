39d7274f5851c5e0f0049b096037ce7cd9284dbc
Ihor Horchynskyi <i.horchynskyi@gmail.com>
2022-01-17 01:13:57 +0200

    Adding libraries and custom script to add them to maven repo

---
+++ additional libs/install.sh
@@ -0,0 +1,7 @@
+mvn install:install-file -Dfile=grain-model.jar -DgroupId=com.eiei.custom-model -DartifactId=grain-model -Dversion=1.7 -Dpackaging=jar
+mvn install:install-file -Dfile=al3d.jar -DgroupId=com.anylogic.custom-model -DartifactId=al3d -Dversion=1 -Dpackaging=jar
+mvn install:install-file -Dfile=engine.jar -DgroupId=com.anylogic.custom-model -DartifactId=engine -Dversion=1 -Dpackaging=jar
+mvn install:install-file -Dfile=engine.nl.jar -DgroupId=com.anylogic.custom-model -DartifactId=engine.nl -Dversion=1 -Dpackaging=jar
+mvn install:install-file -Dfile=engine.sa.jar -DgroupId=com.anylogic.custom-model -DartifactId=engine.sa -Dversion=1 -Dpackaging=jar
+mvn install:install-file -Dfile=process-modeling-library.jar -DgroupId=com.anylogic.custom-model -DartifactId=process-modeling-library -Dversion=1 -Dpackaging=jar
+mvn install:install-file -Dfile=rail-library.jar -DgroupId=com.anylogic.custom-model -DartifactId=rail-library -Dversion=1 -Dpackaging=jar
